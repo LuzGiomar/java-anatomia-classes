2df191a2e9aedc8c2348e84d564882f40abec5fc
“luzaraldi” <devluzaraldi@gmail.com>
2024-07-10 03:03:25 -0300

    first commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class Minhaclasse {
+    
+    public static void main(String[] args) {
+    
+        String primeiroNome = Gleyson; 
+        String segundoNome="Sampaio";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        public static String nomeCompleto (String primeiroNome, String segundoNome) {
+            return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome); 
+        }
+
+    }
+}
